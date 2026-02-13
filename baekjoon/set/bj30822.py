47bbd5ebfac4c88b30d910208a836b9248185b2b
KangIn <macckin0311git@gmail.com>
2026-02-13 13:16:29 +0900

    브론즈 집합

---
+++ baekjoon/set/bj30822.py
@@ -0,0 +1,17 @@
+# UOSPC 세기
+
+n = int(input())
+s = input()
+uospc = [0]*(5)
+for i in s:
+    if i == 'u':
+        uospc[0] += 1
+    if i == 'o':
+        uospc[1] += 1
+    if i == 's':
+        uospc[2] += 1
+    if i == 'p':
+        uospc[3] += 1
+    if i == 'c':
+        uospc[4] += 1
+print(min(uospc))
