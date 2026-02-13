1af0e4e83c9c365202f8285b7cc75fe7d3d181ac
KangIn <macckin0311git@gmail.com>
2026-02-13 18:15:15 +0900

    푸는 중

---
+++ programmers/lvl1택배상자꺼내기.py
@@ -0,0 +1,20 @@
+# 택배상자 꺼내기 
+# 1~n 택배상자
+# 택배상자 쌓고 그 위에 다 뺄때까지 num상자 못뺌
+# 뺄 상자 포함 한 빼는 상자개수
+
+def solution(n, w, num):
+    L = [[0 for _ in range(w-1)]for _ in range(n//w+1)]
+    for i in range(n//w+1): # i 는 행
+        if i % 2 == 0: # 똑바로
+            L[i]
+        else: # 역행
+            
+            
+        
+    
+    answer = 0
+    return answer
+
+
+solution(22,6,8)
